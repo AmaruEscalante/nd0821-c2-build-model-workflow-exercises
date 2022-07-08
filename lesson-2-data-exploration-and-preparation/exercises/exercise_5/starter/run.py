4f905f9f0c0afedbad2330de6cbcb9134262873d
AmaruEscalante <amaruescalante@gmail.com>
2022-07-08 18:17:47 -0500

    feat: make exercise 4 repliclable using a python script

--- lesson-2-data-exploration-and-preparation/exercises/exercise_5/starter/run.py
+++ lesson-2-data-exploration-and-preparation/exercises/exercise_5/starter/run.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+import os
 import argparse
 import logging
 import pandas as pd
@@ -13,9 +14,42 @@ def go(args):
 
     run = wandb.init(project="exercise_5", job_type="process_data")
 
-    ## YOUR CODE HERE
-    pass
+    # Load data
+    logger.info("Loading data...")
+    artifact = run.use_artifact(args.input_artifact)
+    artifact_path = artifact.file()
+    df = pd.read_parquet(artifact_path)
+    
+    # Remove duplicates
+    logger.info("Removing duplicates...")
+    df = df.drop_duplicates().reset_index(drop=True)
+    
+    # Add new feature
+    logger.info("Adding new feature...")
+    df['title'].fillna(value='', inplace=True)
+    df['song_name'].fillna(value='', inplace=True)
+    df['text_feature'] = df['title'] + ' ' + df['song_name']
 
+    # Save data to csv
+    logger.info("Saving data...")
+    filename = "processed_data.csv"
+    df.to_csv(filename)
+
+    artifact = wandb.Artifact(
+        name=args.artifact_name,
+        type=args.artifact_type,
+        description=args.artifact_description,
+    )
+
+    artifact.add_file(filename)
+    # Save data to artifact
+    logger.info("Saving artifact...")
+    run.log_artifact(artifact)
+    
+    # End run
+    logger.info("Ending run...")
+    os.remove(filename)
+    run.finish()
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
